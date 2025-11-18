d42e65c157cecb47ec60e8db6a367208db253e16
Pedro Nunes <pnunes515@protonmail.com>
2025-11-18 09:30:53 +0000

    Fixed build scripts.

--- scripts/build_and_run.bat
+++ scripts/build_and_run.bat
@@ -1,9 +1,9 @@
-cmake --build ../build/debug-windows --target setup_compile_commands game dome forge
+cmake --build ../../build/debug-windows --target setup_compile_commands game dome forge
 if %errorlevel% equ 0 (
     echo Build successful, launching game...
-    cd ..\game\bin
+    cd ..\..\game\bin
     game.exe
-    cd ..\..\scripts
+    cd ..\..\pandora\scripts
 ) else (
     echo Build failed with error code %errorlevel%
 )
