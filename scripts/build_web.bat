a0ad83b5f9ffc594f877535b7863c8a91a932422
Pedro Nunes <pnunes515@protonmail.com>
2025-12-30 14:56:25 +0000

    Support for web release builds.

--- scripts/build_web.bat
+++ scripts/build_web.bat
@@ -1,1 +1,17 @@
-cmake --build ../../build/debug-web --target setup_compile_commands game
+@echo off
+setlocal
+
+if "%1"=="" (
+    echo Usage: build_web.bat [debug^|release]
+    exit /b 1
+)
+
+if /i "%1"=="debug" (
+    cmake --build ../../build/debug-web --target setup_compile_commands game
+) else if /i "%1"=="release" (
+    cmake --build ../../build/release-web --target setup_compile_commands game
+) else (
+    echo Invalid argument: %1
+    echo Usage: build_web.bat [debug^|release]
+    exit /b 1
+)
